a72cde8297837d7534322108c38133f6565d81ad
bwielk <bwielk@gmail.com>
2018-05-03 21:17:16 +0200

    Added classes and colouring to identify threads

---
+++ src/threads/AnotherThread.java
@@ -0,0 +1,11 @@
+package threads;
+
+import static threads.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread{
+
+    @Override
+    public void run(){
+        System.out.println(ANSI_BLUE + "Another thread");
+    }
+}
--- src/threads/ThreadsMain.java
+++ src/threads/ThreadsMain.java
@@ -1,8 +1,22 @@
 package threads;
 
+import static threads.ThreadColor.ANSI_GREEN;
+import static threads.ThreadColor.ANSI_PURPLE;
+
+
 public class ThreadsMain {
 
     public static void main(String[] args) {
-        System.out.println("First thread");
+        System.out.println(ANSI_GREEN + "First thread");
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+
+        new Thread(){
+            public void run(){
+                System.out.println(ANSI_PURPLE + "The anonymous class output");
+            }
+        }.start();
+
+        System.out.println(ANSI_GREEN + "Another notification from the main thread");
     }
 }

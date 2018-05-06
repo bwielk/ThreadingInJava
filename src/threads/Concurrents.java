25cc1429918f4924b5f7f6c89fd8a70428481ac8
bwielk <bwielk@gmail.com>
2018-05-06 23:18:21 +0200

    run methods encapsulated in try finally clauses

--- src/threads/Concurrents.java
+++ src/threads/Concurrents.java
@@ -44,16 +44,22 @@ class MyProducer implements Runnable{
             try{
                 System.out.println(colour + "Adding " + num);
                 bufferLock.lock();
-                buffer.add(num);
-                bufferLock.unlock();
+                try{
+                    buffer.add(num);
+                }finally{
+                    bufferLock.unlock();
+                }
                 Thread.sleep(random.nextInt(1000));
             }catch(InterruptedException e){
                 System.out.println("Producer interrupted");
             }
         }
         bufferLock.lock();
-        buffer.add("EOF");
-        bufferLock.unlock();
+        try{
+            buffer.add("EOF");
+        }finally{
+            bufferLock.unlock();
+        }
     }
 }
 
@@ -72,18 +78,19 @@ class MyConsumer implements Runnable{
     public void run(){
         while(true){
             bufferLock.lock();
-            if(buffer.isEmpty()){
+            try{
+                if(buffer.isEmpty()){
+                    continue;
+                }
+                if(buffer.get(0).equals(EOF)){
+                    System.out.println(color + "Exiting");
+                    break;
+                }else{
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
+            }finally{
                 bufferLock.unlock();
-                continue;
             }
-            if(buffer.get(0).equals(EOF)){
-                System.out.println(color + "Exiting");
-                bufferLock.unlock();
-                break;
-            }else{
-                System.out.println(color + "Removed " + buffer.remove(0));
-            }
-            bufferLock.unlock();
         }
     }
 }
